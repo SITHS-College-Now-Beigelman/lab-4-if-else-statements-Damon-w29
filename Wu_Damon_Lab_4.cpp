f215cab04219e671c9c006a3c11fa4d3b8178f9d
Damon-w29 <Damonw18@nycstudents.net>
2024-10-01 17:03:07 -0400

    "fix"

--- Wu_Damon_Lab_4.cpp
+++ Wu_Damon_Lab_4.cpp
@@ -16,15 +16,10 @@ int main() {
     bool loop = true;
 
     while(loop = true){
-        cout << " Name a fruit and its sugar content in one cup. To stop determining, type 'stop'. \n";
+        cout << "Name a fruit and its sugar content in one cup. To stop determining, type 'stop'. \n";
         cin >> fruit;
         cin >> sugar;
 
-        if (fruit == "stop")
-        {
-            loop = false;
-            cout << "bye bye!" "\n";
-        }
         if(sugar < 10)                                 // if the sugar content per cup is below 10, print its name out and label it as a low sugar
         {
             cout << fruit << " Low sugar fruit \n";
@@ -34,7 +29,13 @@ int main() {
             cout << fruit << " Medium sugar fruit \n";
         }
         else 
-            cout << fruit << "High sugar fruit \n"; // if both settings dont fit, print the name out and label it as a high sugar fruit. 
+            cout << fruit << " High sugar fruit \n"; // if both settings dont fit, print the name out and label it as a high sugar fruit. 
+
+         if (fruit == "stop")
+        {
+            loop = false;
+            cout << "bye bye!" "\n";
+        }
 
     }
     return 0;
